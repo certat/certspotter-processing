94af683e5d81969158579c5d54602bc0d0345b72
Sebastian Wagner <wagner@cert.at>
2020-06-30 10:20:18 +0200

    results: remove Filename and fix tests

--- test.py
+++ test.py
@@ -6,12 +6,11 @@ Some unit tests
 import io
 import os
 import unittest
-import warnings
+from collections import OrderedDict
 
 import config
 import results
 import sending
-
 
 with open(os.path.join(os.path.dirname(__file__),
                        'tests/config-with-recipients.txt')) as handle:
@@ -21,27 +20,25 @@ with open(os.path.join(os.path.dirname(__file__),
     result_string = handle.read()
 
 result_expected = [
-{
-    'id': 'ahPhe7audomoalejaku5LiweNoish5shee3WioW2ahg4cee6Kie5kongih0eiwae',
-    'DNS Name': ['www.example.com'],
-    'Pubkey': 'ThooGh1ahgehae6um4Veik6pofexeeTohmahgeechae0ui1thae6thuzie6gahj1',
-    'Issuer': 'C=US, O=VeriSign, Inc., OU=VeriSign Trust Network, OU=Terms of use at https://www.verisign.com/rpa (c)10, CN=VeriSign Class 3 International Server CA - G3',
-    'Not Before': '2014-01-01 00:00:00 +0000 UTC',
-    'Not After': '2015-01-01 23:59:59 +0000 UTC',
-    'Log Entry': '123 @ https://ct.googleapis.com/pilot (Certificate)',
-    'crt.sh': 'https://crt.sh/?sha256=ahPhe7audomoalejaku5LiweNoish5shee3WioW2ahg4cee6Kie5kongih0eiwae',
-     'Filename': '/home/user/.certspotter/certs/ah/ahPhe7audomoalejaku5LiweNoish5shee3WioW2ahg4cee6Kie5kongih0eiwae.cert.pem',
-     },
-    {'id': 'oe2ga3ShahThiebeing9ookeece9seivae5ciChaa3ooLaew9luoLoh6uyozish7',
-     'DNS Name': ['*.example.at', 'example.at'],
-     'Pubkey': 'XobeLeud1gie3eetaicheiPhamus2Cah4iNohw7OV5gohgu8eiQuei9OoJ7suim1',
-     'Issuer': 'C=DE, O=EUNETIC GmbH, CN=EuropeanSSL Server CA',
-     'Not Before': '2015-01-01 00:00:00 +0000 UTC',
-     'Not After': '2016-01-01 23:59:59 +0000 UTC',
-     'Log Entry': '124 @ https://ct.googleapis.com/pilot (Certificate)',
-     'crt.sh': 'https://crt.sh/?sha256=oe2ga3ShahThiebeing9ookeece9seivae5ciChaa3ooLaew9luoLoh6uyozish7',
-     'Filename': '/home/user/.certspotter/certs/oe/oe2ga3ShahThiebeing9ookeece9seivae5ciChaa3ooLaew9luoLoh6uyozish7.cert.pem'}
-     ]
+    OrderedDict({
+        'id': 'ahPhe7audomoalejaku5LiweNoish5shee3WioW2ahg4cee6Kie5kongih0eiwae',
+        'DNS Name': ['www.example.com'],
+        'Pubkey': 'ThooGh1ahgehae6um4Veik6pofexeeTohmahgeechae0ui1thae6thuzie6gahj1',
+        'Issuer': 'C=US, O=VeriSign, Inc., OU=VeriSign Trust Network, OU=Terms of use at https://www.verisign.com/rpa (c)10, CN=VeriSign Class 3 International Server CA - G3',
+        'Not Before': '2014-01-01 00:00:00 +0000 UTC',
+        'Not After': '2015-01-01 23:59:59 +0000 UTC',
+        'Log Entry': '123 @ https://ct.googleapis.com/pilot (Certificate)',
+        'crt.sh': 'https://crt.sh/?sha256=ahPhe7audomoalejaku5LiweNoish5shee3WioW2ahg4cee6Kie5kongih0eiwae',
+    }),
+    OrderedDict({'id': 'oe2ga3ShahThiebeing9ookeece9seivae5ciChaa3ooLaew9luoLoh6uyozish7',
+                 'DNS Name': ['*.example.at', 'example.at'],
+                 'Pubkey': 'XobeLeud1gie3eetaicheiPhamus2Cah4iNohw7OV5gohgu8eiQuei9OoJ7suim1',
+                 'Issuer': 'C=DE, O=EUNETIC GmbH, CN=EuropeanSSL Server CA',
+                 'Not Before': '2015-01-01 00:00:00 +0000 UTC',
+                 'Not After': '2016-01-01 23:59:59 +0000 UTC',
+                 'Log Entry': '124 @ https://ct.googleapis.com/pilot (Certificate)',
+                 'crt.sh': 'https://crt.sh/?sha256=oe2ga3ShahThiebeing9ookeece9seivae5ciChaa3ooLaew9luoLoh6uyozish7'})
+]
 config_expected = {'example.com': 'abc@example.com',
                    'example.net': 'abc@example.com',
                    'example.at': 'abc@example.com',
@@ -50,7 +47,7 @@ config_expected = {'example.com': 'abc@example.com',
                    }
 sending_expected = {
     'abc@example.com': result_expected,
-    }
+}
 
 
 class TestCertspotterConfig(unittest.TestCase):
@@ -68,6 +65,7 @@ class TestCertspotterConfig(unittest.TestCase):
 
 
 class TestCertspotterResult(unittest.TestCase):
+    maxDiff = None
 
     def test_result_reader_no_data(self):
         result = list(results.read_data(''))
